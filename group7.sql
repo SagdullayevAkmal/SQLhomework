756d26d90d0c9f3aca908e1418ded5e194532946
SagdullayevAkmal <mt7351535@gmail.com>
2025-05-06 00:40:04 +0500

    group7

---
+++ group7.sql
@@ -0,0 +1,8 @@
+create database group7
+use group7
+create table students(id int,firstname varchar(50),familyname varchar(50),age int,)
+select *from students
+insert into students
+(id,firstname,familyname,age)
+values
+('01','jones','fred','21')
